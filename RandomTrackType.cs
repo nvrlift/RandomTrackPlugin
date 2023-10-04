423364cd5f2304712e0757030283095d5916b9a3
Nico Macioszek <nico.macioszek@untitled.software>
2023-10-05 01:09:56 +0200

    more changes i thought were commited already

--- RandomTrackType.cs
+++ RandomTrackType.cs
@@ -4,9 +4,14 @@ namespace RandomTrackPlugin;
 
 public class RandomTrackType : TrackBaseType
 {
+    public float Weight { get; set; } = 1.0f;
     public RandomTrackType(WeightEntry input)
     {
         Name = input.Name;
-        
+        TrackFolder = input.TrackFolder;
+        TrackLayoutConfig = input.TrackLayoutConfig;
+        CMLink = input.CMLink ?? "";
+        CMVersion = input.CMVersion ?? "";
+        Weight = input.Weight ?? 1.0f;
     }
 }
